4f16ca2cbde99dd54cb5be0adbfc5f6b6fb97326
lcollazo <lcollazo@pbcgov.org>
2026-03-09 14:43:02 -0400

    fix(InputOtp): prevent invalid characters based on InputMode

--- src/BlazorBlueprint.Components/wwwroot/js/input-otp.js
+++ src/BlazorBlueprint.Components/wwwroot/js/input-otp.js
@@ -11,8 +11,9 @@ const instances = new Map();
  * @param {HTMLElement} element - The OTP container element.
  * @param {DotNetObject} dotNetRef - Reference to the Blazor component.
  * @param {string} instanceId - Unique ID for this instance.
+ * @param {string} otpMode - csharp enum InputOTPInputMode (Numbers, Letters, LettersAndNumbers).
  */
-export function initialize(element, dotNetRef, instanceId) {
+export function initialize(element, dotNetRef, instanceId, otpMode) {
     if (!element || !dotNetRef) return;
 
     const handlePaste = (event) => {
@@ -24,7 +25,49 @@ export function initialize(element, dotNetRef, instanceId) {
     };
 
     element.addEventListener('paste', handlePaste);
-    instances.set(instanceId, { element, handlePaste });
+
+    const handleOnKeyDown = (event) => {
+      const otpMode = instances.get(instanceId)?.otpMode;
+
+      if (_isControlKey(event.key)) {
+        return; // Allow control keys (e.g. Backspace, Arrow keys)
+      }
+      else if (event.isComposing) {
+        return; // Allow IME composition
+      }
+      else if (event.ctrlKey || event.metaKey)
+        return; // Allow paste via Ctrl+V or Cmd+V
+      else if (otpMode) {
+        if (!_isCharAllowed(event.key, otpMode)) {
+          event.preventDefault();
+        }
+      }
+
+    };
+
+    element.addEventListener('keydown', handleOnKeyDown);
+
+    instances.set(instanceId, { element, handlePaste, otpMode });
+}
+
+export function updateOtpMode(instanceId, otpMode) {
+  const instance = instances.get(instanceId);
+  if (!instance) return;
+  instance.otpMode = otpMode;
+}
+
+function _isControlKey(key) {
+  return key.length !== 1; // Since control keys (e.g. Backspace, Arrow keys) typically have a key value longer than 1 character
+}
+function _isCharAllowed(char, otpMode) {
+  if (otpMode === 'Numbers') {
+    return /\d/.test(char);
+  } else if (otpMode === 'Letters') {
+    return /[a-zA-Z]/.test(char);
+  } else if (otpMode === 'LettersAndNumbers') {
+    return /[a-zA-Z0-9]/.test(char);
+  }
+  console.warn(`Unknown OTP mode: ${otpMode}`);
 }
 
 /**
@@ -35,5 +78,6 @@ export function dispose(instanceId) {
     const instance = instances.get(instanceId);
     if (!instance) return;
     instance.element.removeEventListener('paste', instance.handlePaste);
+    instance.element.removeEventListener('keydown', instance.handleOnKeyDown);
     instances.delete(instanceId);
 }
